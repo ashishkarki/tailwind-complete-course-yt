2cbccc87af818e28239d31c6596933aa6865ffc1
Ashish Karki <ashishk@Ashishs-MacBook-Pro.local>
2022-02-12 21:30:49 +0545

    feat(tailwind-features): :construction: done finishing this tutorial..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
+  mode: 'jit',
   content: ['./src/**/*.{html,js,css}', './public/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1100px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      const utilities = {
+        '.scale-1': {
+          transform: 'scale(1)',
+        },
+      }
+
+      addUtilities(utilities, ['responsive'])
+    }),
+  ],
 }
